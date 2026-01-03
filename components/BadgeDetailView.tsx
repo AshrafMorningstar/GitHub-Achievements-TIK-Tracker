fcf18bd2de39afe6fc213808e9f58d5f8333e200
Ashraf Morningstar <ashrafmorningstar@gmail.com>
2026-01-03 17:01:45 +0530

    chore(copyright): update headers and license information © 2026

--- components/BadgeDetailView.tsx
+++ components/BadgeDetailView.tsx
@@ -1,3 +1,11 @@
+/*
+ Copyright (c) 2026 Ashraf Morningstar
+ These are personal recreations of existing projects, developed by Ashraf Morningstar
+ for learning and skill development.
+ Original project concepts remain the intellectual property of their respective creators.
+ Repository: https://github.com/AshrafMorningstar
+*/
+
 import React, { useEffect } from 'react';
 import { Achievement } from '../types';
 import { ArrowLeft, Star, CheckCircle2, Lock, Trophy, ExternalLink } from 'lucide-react';
--- data.ts
+++ data.ts
@@ -1,3 +1,11 @@
+/*
+ Copyright (c) 2026 Ashraf Morningstar
+ These are personal recreations of existing projects, developed by Ashraf Morningstar
+ for learning and skill development.
+ Original project concepts remain the intellectual property of their respective creators.
+ Repository: https://github.com/AshrafMorningstar
+*/
+
 import { Achievement, Rarity } from './types';
 
 // Using raw.githubusercontent.com from the reference repo (drknzz/GitHub-Achievements) or similar reliable sources for badge images.
